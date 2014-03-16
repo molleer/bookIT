e7f4048006b05eb564fc8cf78d600d28ebab9c94
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-03-16 03:12:57 +0100

    put out some fires when not signed in

--- lib/it_auth.rb
+++ lib/it_auth.rb
@@ -4,16 +4,22 @@ require 'JSON'
 class ItAuth
 	@@base_url = "https://chalmers.it/auth/userInfo.php"
 
-	%w[cid dn firstname lastname mail nick uidnumber groups].each do |name|
+	%w[cid dn firstname lastname mail nick uidnumber].each do |name|
 		define_method name do
-			@data[name]
+			@data[name] unless @data.nil?
 		end
 	end
 
+	def groups
+		return @data && @data['groups'] || Array.new
+	end
+
 	def initialize(token)
-		uri = URI.parse(@@base_url)
-		uri.query = "token=#{token}"
-		json = Net::HTTP.get(uri)
-		@data = (json[0] == "{" && JSON.parse(json)) || nil
+		unless token.nil?
+			uri = URI.parse(@@base_url)
+			uri.query = "token=#{token}"
+			json = Net::HTTP.get(uri)
+			@data = (json[0] == "{" && JSON.parse(json)) || Hash.new
+		end
 	end
 end
