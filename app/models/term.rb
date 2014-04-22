9a14a65f77a03fad03b1cb2c5302cd015d42176d
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-04-22 23:41:55 +0200

    term annotated

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: terms
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  content    :text
+#  active     :boolean
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Term < ActiveRecord::Base
 	scope :active, where(active: true)
 end
