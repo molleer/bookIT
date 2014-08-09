88fdd91a39ecd5d4761a6bc3267831d27a4a0822
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-08-09 19:52:58 +0200

    allow framing from chalmers.it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_action :allow_iframe
+
 
   def current_user
     if session[:cookie] == cookies[:chalmersItAuth] && session[:user].present?
@@ -16,4 +18,9 @@ class ApplicationController < ActionController::Base
     @user
   end
   helper_method :current_user
+
+  private
+    def allow_iframe
+      response.headers['X-Frame-Options'] = 'ALLOW-FROM https://chalmers.it'
+    end
 end
