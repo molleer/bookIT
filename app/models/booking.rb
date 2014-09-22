60024a756aa0a91cff6ab0d345c5dea6abc109ed
Johan Lindskogen <johan.lindskogen@gmail.com>
2014-09-22 23:30:06 +0200

    fixed group filter

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -22,7 +22,7 @@
 #
 
 class Booking < ActiveRecord::Base
-  scope :by_group_or_user, -> (name) { where('user_id = ? OR "group" = ?', name, name) }
+  scope :by_group_or_user, -> (name) { where('user_id = ? OR `group` = ?', name, name) }
   scope :in_future, -> { where('end_date >= ?', DateTime.now) }
   scope :within, -> (time = 1.month.from_now) { where('begin_date <= ?', time) }
   scope :waiting, -> { where('accepted IS NULL') }
