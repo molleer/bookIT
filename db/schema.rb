2eef7a048e98e78addd2e35328dc822102567f8d
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-04-20 11:48:17 +0200

    Sorted and added sent_at

--- db/schema.rb
+++ db/schema.rb
@@ -11,104 +11,116 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419162002) do
+ActiveRecord::Schema.define(version: 20150420093054) do
 
   create_table "bookings", force: :cascade do |t|
-    t.string   "user_id"
+    t.string   "user_id",     limit: 255
     t.datetime "begin_date"
     t.datetime "end_date"
-    t.string   "group"
-    t.text     "description"
-    t.integer  "room_id"
+    t.string   "group",       limit: 255
+    t.text     "description", limit: 65535
+    t.integer  "room_id",     limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title"
-    t.string   "phone"
+    t.string   "title",       limit: 255
+    t.string   "phone",       limit: 255
     t.datetime "deleted_at"
   end
 
-  add_index "bookings", ["deleted_at"], name: "index_bookings_on_deleted_at"
-  add_index "bookings", ["room_id"], name: "index_bookings_on_room_id"
+  add_index "bookings", ["deleted_at"], name: "index_bookings_on_deleted_at", using: :btree
+  add_index "bookings", ["room_id"], name: "index_bookings_on_room_id", using: :btree
+
+  create_table "it_room_bookings", force: :cascade do |t|
+    t.datetime "created_at",                     null: false
+    t.string   "title",         limit: 255,      null: false
+    t.text     "description",   limit: 16777215
+    t.string   "phone",         limit: 255,      null: false
+    t.string   "location",      limit: 255,      null: false
+    t.string   "booking_group", limit: 255
+    t.datetime "start_time",                     null: false
+    t.datetime "end_time",                       null: false
+    t.integer  "user_id",       limit: 8,        null: false
+  end
 
   create_table "party_reports", force: :cascade do |t|
-    t.integer  "booking_id"
-    t.string   "party_responsible_name"
-    t.string   "party_responsible_phone"
-    t.string   "party_responsible_mail"
-    t.string   "co_party_responsible_name"
-    t.string   "co_party_responsible_phone"
-    t.string   "co_party_responsible_mail"
-    t.boolean  "liquor_license"
-    t.boolean  "sent"
-    t.boolean  "accepted"
+    t.integer  "booking_id",                 limit: 4
+    t.string   "party_responsible_name",     limit: 255
+    t.string   "party_responsible_phone",    limit: 255
+    t.string   "party_responsible_mail",     limit: 255
+    t.string   "co_party_responsible_name",  limit: 255
+    t.string   "co_party_responsible_phone", limit: 255
+    t.string   "co_party_responsible_mail",  limit: 255
+    t.boolean  "liquor_license",             limit: 1
+    t.boolean  "accepted",                   limit: 1
     t.datetime "begin_date"
     t.datetime "end_date"
     t.datetime "deleted_at"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.datetime "sent_at"
   end
 
-  add_index "party_reports", ["booking_id"], name: "index_party_reports_on_booking_id"
-  add_index "party_reports", ["deleted_at"], name: "index_party_reports_on_deleted_at"
+  add_index "party_reports", ["booking_id"], name: "index_party_reports_on_booking_id", using: :btree
+  add_index "party_reports", ["deleted_at"], name: "index_party_reports_on_deleted_at", using: :btree
 
   create_table "rooms", force: :cascade do |t|
-    t.string   "name"
-    t.boolean  "allow_party"
+    t.string   "name",        limit: 255
+    t.boolean  "allow_party", limit: 1
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "only_group"
+    t.boolean  "only_group",  limit: 1
   end
 
   create_table "rooms_rules", id: false, force: :cascade do |t|
-    t.integer "room_id"
-    t.integer "rule_id"
+    t.integer "room_id", limit: 4
+    t.integer "rule_id", limit: 4
   end
 
-  add_index "rooms_rules", ["room_id"], name: "index_rooms_rules_on_room_id"
-  add_index "rooms_rules", ["rule_id"], name: "index_rooms_rules_on_rule_id"
+  add_index "rooms_rules", ["room_id"], name: "index_rooms_rules_on_room_id", using: :btree
+  add_index "rooms_rules", ["rule_id"], name: "index_rooms_rules_on_rule_id", using: :btree
 
   create_table "rules", force: :cascade do |t|
-    t.integer  "day_mask"
+    t.integer  "day_mask",   limit: 4
     t.datetime "start_date"
     t.datetime "stop_date"
     t.time     "start_time"
     t.time     "stop_time"
-    t.boolean  "allow"
-    t.integer  "prio"
-    t.text     "reason"
-    t.string   "title"
+    t.boolean  "allow",      limit: 1
+    t.integer  "prio",       limit: 4
+    t.text     "reason",     limit: 65535
+    t.string   "title",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "terms", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.boolean  "active"
+    t.string   "title",      limit: 255
+    t.text     "content",    limit: 65535
+    t.boolean  "active",     limit: 1
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", id: false, force: :cascade do |t|
-    t.string   "cid"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "nick"
-    t.string   "mail"
+    t.string   "cid",        limit: 255
+    t.string   "first_name", limit: 255
+    t.string   "last_name",  limit: 255
+    t.string   "nick",       limit: 255
+    t.string   "mail",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "whitelist_items", force: :cascade do |t|
-    t.string   "title"
+    t.string   "title",        limit: 255
     t.time     "begin_time"
     t.time     "end_time"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "days_in_week"
+    t.integer  "days_in_week", limit: 4
     t.date     "rule_start"
     t.date     "rule_end"
-    t.boolean  "blacklist"
+    t.boolean  "blacklist",    limit: 1
   end
 
 end
