8d4c0c7229fa8558c3e8a65de5365a0139436fbb
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-04-22 22:19:56 +0200

    Remove threading
    Increases wait time, but might make it work

--- app/controllers/party_reports_controller.rb
+++ app/controllers/party_reports_controller.rb
@@ -42,9 +42,9 @@ class PartyReportsController < ApplicationController
 
   def send_bookings
     @report = PartyReport.find(params[:report_ids])
-    Thread.new do
+    # Thread.new do
       StudentPortalReporter.new.party_report(@report)
-    end
+    # end
     # AdminMailer.chalmers_message(params[:message]).deliver
 
     PartyReport.where(id: params[:report_ids]).update_all(sent_at: Time.zone.now)
