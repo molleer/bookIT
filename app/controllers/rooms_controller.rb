0eabf74d894e675a733947ba4b10cb11e30cca04
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-05-14 01:36:12 +0200

    Rooms fixup

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -73,7 +73,7 @@ class RoomsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_room
-      @room = Room.find(params[:id])
+      @room = Room.find_by(name: params[:id].downcase)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
