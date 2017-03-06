9745f1d2d45ff4a47bd92d38c2a7265bc089c6f4
Joakim Hulthe <joakim@hulthe.net>
2017-03-06 14:24:14 +0100

    Add FikIT to allowed groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
 	base_uri "https://account.chalmers.it/userInfo.php"
 
 	@@ADMIN_GROUPS = [:digit, :prit, :styrit]
-	@@FILTER = [:digit, :styrit, :prit, :nollkit, :sexit, :fanbarerit, :'8bit', :drawit, :armit, :hookit, :fritid, :snit, :flashit, :valberedningen, :laggit]
+	@@FILTER = [:digit, :styrit, :prit, :nollkit, :sexit, :fanbarerit, :'8bit', :drawit, :armit, :hookit, :fritid, :snit, :flashit, :valberedningen, :laggit, :fikit]
 
 
 	def groups
@@ -88,7 +88,7 @@ class Symbol
 		case self
 			when :digit, :styrit, :sexit, :fritid, :snit
 				self.to_s.gsub /it/, 'IT'
-			when :drawit, :armit, :hookit, :flashit, :laggit
+			when :drawit, :armit, :hookit, :flashit, :laggit, :fikit
 				self.to_s.titleize.gsub /it/, 'IT'
 			when :'8bit'
 				'8-bIT'
