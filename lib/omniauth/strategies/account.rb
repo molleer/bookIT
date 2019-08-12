50f8178400bdd82e63ad2489c928bd2a91565f25
gurr1 <gustav.engsmyre@gmail.com>
2019-08-13 01:41:47 +0200

    Change oauth2 connection details to suite gammas format

--- lib/omniauth/strategies/account.rb
+++ lib/omniauth/strategies/account.rb
@@ -1,3 +1,4 @@
+require 'base64'
 module OmniAuth
   module Strategies
     class Account < OmniAuth::Strategies::OAuth2
@@ -5,19 +6,32 @@ module OmniAuth
 
       option :client_options, {
         site: Rails.configuration.account_ip,
-        authorize_url: 'api/oauth/authorize'
+        authorize_url: 'api/oauth/authorize',
+        token_url: '/api/oauth/token'
       }
 
       uid do
-        raw_info['uid']
+        raw_info['cid']
+      end
+
+      def build_access_token
+        options.token_params.merge!(:headers => {
+            'Authorization' => basic_auth_header,
+            'Content-Type' => "application/x-www-form-urlencoded"
+        })
+        super
+      end
+
+      def basic_auth_header
+        "Basic " + Base64.strict_encode64("#{options[:client_id]}:#{options[:client_secret]}")
       end
 
       info do
-        { name: raw_info["display_name"] }
+        { name: raw_info["nick"] }
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/me.json').parsed
+        @raw_info ||= access_token.get('/api/users/me.json').parsed
       end
     end
   end
