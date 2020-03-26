ed405612dc5fc09a35656d260890ec413884ddfb
Theodor Angergård <portalprogramming@gmail.com>
2020-03-26 18:43:29 +0100

    Create api scripts for reservations in frontend

---
+++ frontend/src/api/reservations/get.reservations.js
@@ -0,0 +1,5 @@
+import { getRequest } from "../utils/api";
+
+export const getReservations = () => getRequest("/reservation/");
+
+export const getReservation = id => getRequest("/reservation/" + id);
