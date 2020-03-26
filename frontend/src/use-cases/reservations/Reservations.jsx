cfcd6400f5e11edda2693dbc4337261cb9784c70
Theodor Angergård <portalprogramming@gmail.com>
2020-03-26 18:39:07 +0100

    Create reservation use case

---
+++ frontend/src/use-cases/reservations/Reservations.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { DigitCRUD } from "@cthit/react-digit-components";
+
+const Reservations = () => {
+    return <DigitCRUD name={"reservations"} path={"/reservations"} />;
+};
+
+export default Reservations;
