f3132c6aa64cc13c80f11431a59f7caa4c404433
Theodor Angergård <portalprogramming@gmail.com>
2020-03-26 18:42:53 +0100

    Add Rooms and Reservation use-case in App

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,39 +1,49 @@
 import React, { useEffect } from "react";
-import { DigitHeader, DigitButton } from "@cthit/react-digit-components";
+import { DigitHeader, DigitNavLink } from "@cthit/react-digit-components";
 import { Route, Switch, useHistory } from "react-router-dom";
-import Rooms from "../use-cases/rooms";
 import { postRequest } from "../api/utils/api";
+import Rooms from "../use-cases/rooms";
+import Reservations from "../use-cases/reservations";
 
 function App() {
-  const history = useHistory();
+    const history = useHistory();
 
-  useEffect(() => {
-    const paramsResponse = new URLSearchParams(window.location.search);
-    const code = paramsResponse.get("code");
-    if (code) {
-      postRequest("/auth/account/callback", { code })
-        .then(() => {
-          history.push("/");
-        })
-        .catch(error => {
-          history.push("/");
-          console.log("Someting went wrong");
-          console.log(error);
-        });
-    }
-  }, [window.location.search]);
+    useEffect(() => {
+        const paramsResponse = new URLSearchParams(window.location.search);
+        const code = paramsResponse.get("code");
+        if (code) {
+            postRequest("/auth/account/callback", { code })
+                .then(() => {
+                    history.push("/");
+                })
+                .catch(error => {
+                    history.push("/");
+                    console.log("Someting went wrong");
+                    console.log(error);
+                });
+        }
+    }, [window.location.search]);
 
-  return (
-    <DigitHeader
-      title={"BookIT"}
-      renderMain={() => (
-        <Switch>
-          <Route path={"/rooms"} component={Rooms} />
-          <DigitButton onClick={() => history.push("/rooms")} text={"hej"} />
-        </Switch>
-      )}
-    />
-  );
+    return (
+        <DigitHeader
+            title={"BookIT"}
+            renderDrawer={() => (
+                <>
+                    <DigitNavLink
+                        link={"/reservations"}
+                        text={"Reservations"}
+                    />
+                    <DigitNavLink link={"/rooms"} text={"Rooms"} />
+                </>
+            )}
+            renderMain={() => (
+                <Switch>
+                    <Route path={"/reservations"} component={Reservations} />
+                    <Route path={"/rooms"} component={Rooms} />
+                </Switch>
+            )}
+        />
+    );
 }
 
 export default App;
