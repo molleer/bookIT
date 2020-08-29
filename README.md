622396a30aebabf8cab61ee0aa6ea0f771acefef
David Möller <davidm@student.chalmers.se>
2020-08-29 18:19:36 +0200

    Added a full callendar to reservations

--- README.md
+++ README.md
@@ -1,71 +1,11 @@
 # bookIT
 
-Bokningssystem för Chalmers IT  
-*Byggt i Ruby on Rails av Ndushi*
+## Calendar docs
 
-Features:
-
- * Administration av bokningar + arrangemangsanmälningar (endast VO)
- * Regelsystem med matchningar mot dynamiskt definerade regler
-
-
-Kör följande kommando för att skapa 'Hubben' och 'Grupprummet':
-
-```
-rake db:seed
-```
-
-### Setup med docker:
-för att bygga en lokal utvecklings miljö:
-```
-docker-compose up --build
-```
-För att lösa Autentiering mot accounts, lägg till:
-```
-0.0.0.0       local.chalmers.it
-```
-i `/etc/hosts`
-
-För att starta:
-```
-docker-compose up
-```
-"bookIT" finns sedan på:  
-"local.chalmers.it:3000"
-
-### rake tasks
-Lägg till följande till cron: `cd path_to_bookit && path_to_rake RAILS_ENV=production bookit:remind`
-Bör köras varje timme
-
-
-* `rake bookit:parse`, `rake bookit:clean`, `rake bookit:reparse`
-	* Parsear [Chalmers Läsårstider](https://www.student.chalmers.se/sp/academic_year_list) efter läsperiodstider
-	* Skapa `config/matchers.rb` för att definera vad de innebär
-
-
-#### Syntax för `config/matchers.rb`:
-
-```
-match regexp, times_array [, options]
-```
-
-##### Times array:
-Array of strings in this format: `HH:MM-HH:MM`
-
-##### Options:
-Possible keys:
-
-* `:blacklist`
-	*  `(boolean)`
-* `:title`
-	*  `(string)`
-* `:days`
-	* An array of weekdays: `[:mon, :tue, :wed, :thu, :fri, :sat, :sun]`
-	* or a shorthand value `:weekdays`, `:weekends`, `:all`.
-
-
+-   [Full Calendar](https://fullcalendar.io/docs)
 
 ##### Exempel:
+
 ```
 # Mellan 15-00 & 11-12 på helger får man inte boka (under läsperioder)
 match /Läsperiod \d/, ['15:00-24:00', '11:00-12:00'], days: :weekends, blacklist: true
--- frontend/src/use-cases/reservations/Reservations.jsx
+++ frontend/src/use-cases/reservations/Reservations.jsx
@@ -1,8 +1,65 @@
 import React from "react";
-import { DigitCRUD } from "@cthit/react-digit-components";
+import FullCallendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { DigitDesign } from "@cthit/react-digit-components";
 
 const Reservations = () => {
-    return <DigitCRUD name={"reservations"} path={"/reservations"} />;
+    return (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <DigitDesign.Card
+                size={{ width: "95%" }}
+                style={{ marginTop: "2rem" }}
+            >
+                <DigitDesign.CardBody>
+                    <FullCallendar
+                        eventTimeFormat={{
+                            hour: "2-digit",
+                            minute: "2-digit",
+                            hour12: false,
+                        }}
+                        slotLabelFormat={{
+                            hour: "2-digit",
+                            minute: "2-digit",
+                            hour12: false,
+                        }}
+                        rerenderDelay={1000}
+                        allDaySlot
+                        weekNumbers
+                        editable
+                        headerToolbar={{
+                            start: "title",
+                            center: "",
+                            end: "today,prev,next",
+                        }}
+                        plugins={[dayGridPlugin, timeGridPlugin]}
+                        initialView="timeGridWeek"
+                        eventOverlap
+                        events={[
+                            {
+                                title: "Styrit Möte",
+                                start: "2020-08-29T06:00",
+                                end: "2020-08-29T07:00",
+                                display: "block",
+                            },
+                            {
+                                title: "Styrit Möte",
+                                start: "2020-08-29T12:00",
+                                end: "2020-08-29T13:00",
+                                display: "block",
+                            },
+                            {
+                                title: "Styrit Möte",
+                                start: "2020-08-29T12:30",
+                                end: "2020-08-29T13:30",
+                                display: "block",
+                            },
+                        ]}
+                    />
+                </DigitDesign.CardBody>
+            </DigitDesign.Card>
+        </div>
+    );
 };
 
 export default Reservations;
