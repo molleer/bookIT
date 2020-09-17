0f59c4e0877868a4ffbe72966efeec1ff6f9a6ed
David Möller <davidm@student.chalmers.se>
2020-09-17 15:54:46 +0200

    Added notification list in admin view

--- frontend/src/use-cases/admin/Admin.jsx
+++ frontend/src/use-cases/admin/Admin.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import {
+    DigitList,
+    DigitDesign,
+    DigitText,
+    DigitIconButton,
+} from "@cthit/react-digit-components";
+import MailIcon from "@material-ui/icons/Mail";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddIcon from "@material-ui/icons/Add";
 
 const Admin = () => {
-    return <div>Admin</div>;
+    return (
+        <div>
+            <DigitDesign.Card>
+                <DigitDesign.CardBody>
+                    <DigitList
+                        title="Notifikation vid aktivitetsanälan"
+                        items={[
+                            {
+                                text: "vo@chalmers.it",
+                                icon: MailIcon,
+                                actionIcon: DeleteIcon,
+                            },
+                            {
+                                text: "ordf.prit@chalmers.it",
+                                icon: MailIcon,
+                                actionIcon: DeleteIcon,
+                            },
+                        ]}
+                    />
+                </DigitDesign.CardBody>
+                <DigitDesign.CardButtons style={{ "justify-content": "right" }}>
+                    <DigitIconButton icon={AddIcon} primary />
+                </DigitDesign.CardButtons>
+            </DigitDesign.Card>
+        </div>
+    );
 };
 
 export default Admin;
